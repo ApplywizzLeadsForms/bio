61bf640f630621e971e79fd9ee08eb7c8d8b0e61
ApplywizzLeadsForms <applywizzleadsforms@gmail.com>
2025-08-13 03:59:04 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,11 +1,3 @@
-// // src/lib/supabaseClient.ts
-// import { createClient } from '@supabase/supabase-js';
-
-// export const supabase = createClient(
-//   'https://mrsmhqgdwjopasnpohwu.supabase.co',
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yc21ocWdkd2pvcGFzbnBvaHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0NDcwNTAsImV4cCI6MjA2NzAyMzA1MH0.gT89NQPLbOQi0B5P0lT6PKqbH-7TMXBmA8IXrGtkT8o'
-// );
-
 
 // src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
